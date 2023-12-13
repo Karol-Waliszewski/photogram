8ced3d94dff6691137cf0be4142787c23fd2b099
Karol-Waliszewski <karolwaliszewski@gmail.com>
2023-12-13 16:57:41 +0100

    Added forwardRef to Typography

--- src/components/atoms/Typography.tsx
+++ src/components/atoms/Typography.tsx
@@ -1,115 +1,106 @@
 import { cn } from "@/utils/cn";
-import { type PropsWithChildren } from "react";
+import { forwardRef } from "react";
 
-export type TypographyProps = React.HTMLAttributes<HTMLParagraphElement> &
-  React.HTMLAttributes<HTMLHeadingElement>;
-
-export const H1 = ({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<TypographyProps>) => (
+export const H1 = forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
   <h1
+    ref={ref}
     className={cn(
       "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
       className,
     )}
     {...props}
-  >
-    {children}
-  </h1>
-);
+  />
+));
+H1.displayName = "H1";
 
-export const H2 = ({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<TypographyProps>) => (
+export const H2 = forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
   <h2
+    ref={ref}
     className={cn(
       "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
       className,
     )}
     {...props}
-  >
-    {children}
-  </h2>
-);
+  />
+));
+H2.displayName = "H2";
 
-export const H3 = ({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<TypographyProps>) => (
+export const H3 = forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
   <h3
+    ref={ref}
     className={cn(
       "scroll-m-20 text-2xl font-semibold tracking-tight",
       className,
     )}
     {...props}
-  >
-    {children}
-  </h3>
-);
+  />
+));
+H3.displayName = "H3";
 
-export const H4 = ({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<TypographyProps>) => (
+export const H4 = forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
   <h4
+    ref={ref}
     className={cn(
       "scroll-m-20 text-xl font-semibold tracking-tight",
       className,
     )}
     {...props}
-  >
-    {children}
-  </h4>
-);
+  />
+));
+H4.displayName = "H4";
 
-export const Text = ({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<TypographyProps>) => (
+export const Text = forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
   <p
+    ref={ref}
     className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
     {...props}
-  >
-    {children}
-  </p>
-);
+  />
+));
+Text.displayName = "Text";
 
-export const Large = ({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<TypographyProps>) => (
-  <p className={cn("text-lg font-semibold", className)} {...props}>
-    {children}
-  </p>
-);
+export const Large = forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p ref={ref} className={cn("text-lg font-semibold", className)} {...props} />
+));
+Large.displayName = "Large";
 
-export const Small = ({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<TypographyProps>) => (
+export const Small = forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
   <small
+    ref={ref}
     className={cn("text-sm font-medium leading-none", className)}
     {...props}
-  >
-    {children}
-  </small>
-);
+  />
+));
+Small.displayName = "Small";
 
-export const Muted = ({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<TypographyProps>) => (
-  <p className={cn("text-muted-foreground text-sm", className)} {...props}>
-    {children}
-  </p>
-);
+export const Muted = forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn("text-muted-foreground text-sm", className)}
+    {...props}
+  />
+));
+Muted.displayName = "Muted";
