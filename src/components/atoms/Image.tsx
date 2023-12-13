16ffeaaf32752943f17d3483d517eb665188f960
Karol-Waliszewski <karolwaliszewski@gmail.com>
2023-12-13 19:11:40 +0100

    Improved Image atom

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,5 +1,43 @@
+// https://nextjs.org/docs/pages/api-reference/components/image
+
 import NextImage from "next/image";
+import { type TypedOmit } from "@/utils/types";
+import { cn } from "@/utils/cn";
 
-const Image = NextImage;
+type NextImageProps = React.ComponentProps<typeof NextImage>;
+export type ImageProps = TypedOmit<
+  NextImageProps,
+  "width" | "height" | "fill" | "placeholder"
+> &
+  (
+    | {
+        width: number;
+        height: number;
+      }
+    | { cover: boolean }
+    | { responsive: boolean }
+  );
 
-export { Image };
+export const Image: React.FC<ImageProps> = ({ className, ...props }) => {
+  if ("responsive" in props && props.responsive) {
+    const { responsive, ...rest } = props;
+    return (
+      <div className={cn("relative h-full w-full", className)}>
+        <NextImage {...rest} fill={responsive} />
+      </div>
+    );
+  }
+
+  if ("cover" in props && props.cover) {
+    const { cover, ...rest } = props;
+    return (
+      <NextImage
+        {...rest}
+        fill={cover}
+        className={cn("object-cover", className)}
+      />
+    );
+  }
+
+  return <NextImage className={className} {...props} />;
+};
