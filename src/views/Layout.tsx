c3fe68d2b0f5f181d5aeef60496ffbf6072f61da
Karol-Waliszewski <karolwaliszewski@gmail.com>
2023-12-15 14:50:28 +0100

    Add sidebar and scrollarea to Layout component

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,6 +1,51 @@
-import { Container } from "@/components/atoms/Container";
-import { cn } from "@/utils/cn";
 import { type PropsWithChildren } from "react";
+import { LogIn, LogOut, UserRound, UserRoundPlus, Image } from "lucide-react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+import { Container } from "@/components/atoms/Container";
+import { ScrollArea } from "@/components/atoms/ScrollArea";
+import { Sidebar, type SidebarSection } from "@/components/molecules/Sidebar";
+
+import { cn } from "@/utils/cn";
+
+const SIDEBAR_SECTIONS_UNAUTHENTICATED: SidebarSection[] = [
+  {
+    title: "Posts",
+    items: [{ type: "link", label: "All posts", href: "/", icon: Image }],
+  },
+  {
+    title: "User",
+    items: [
+      { type: "button", label: "Login", action: () => signIn(), icon: LogIn },
+    ],
+  },
+];
+const SIDEBAR_SECTIONS_AUTHENTICATED: SidebarSection[] = [
+  {
+    title: "Posts",
+    items: [
+      { type: "link", label: "All posts", href: "/", icon: Image },
+      {
+        type: "link",
+        label: "Following posts",
+        href: "/following",
+        icon: UserRoundPlus,
+      },
+    ],
+  },
+  {
+    title: "User",
+    items: [
+      { type: "link", label: "Profile", href: "/profile", icon: UserRound },
+      {
+        type: "button",
+        label: "Logout",
+        action: () => signOut(),
+        icon: LogOut,
+      },
+    ],
+  },
+];
 
 export type LayoutProps = React.HTMLAttributes<HTMLDivElement>;
 export const Layout = ({
@@ -8,9 +53,26 @@ export const Layout = ({
   className,
   ...props
 }: PropsWithChildren<LayoutProps>) => {
+  const { status } = useSession();
   return (
-    <Container className={cn("py-4", className)} variant={"slim"} {...props}>
-      {children}
-    </Container>
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar
+        sections={
+          status === "authenticated"
+            ? SIDEBAR_SECTIONS_AUTHENTICATED
+            : SIDEBAR_SECTIONS_UNAUTHENTICATED
+        }
+        className="w-1/3 max-w-[250px]"
+      />
+      <ScrollArea className="max-h-screen w-full overflow-auto">
+        <Container
+          className={cn("py-4", className)}
+          variant={"slim"}
+          {...props}
+        >
+          {children}
+        </Container>
+      </ScrollArea>
+    </div>
   );
 };
