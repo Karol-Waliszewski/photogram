9b13f336a59b2c308aaa8fdf7bbb53158c8c93e6
Karol-Waliszewski <karolwaliszewski@gmail.com>
2023-12-21 21:22:18 +0100

    Connected new post dialog to react-hook-form

--- src/components/organisms/NewPostDialog.tsx
+++ src/components/organisms/NewPostDialog.tsx
@@ -1,4 +1,8 @@
 import { type Accept } from "react-dropzone";
+import { cn } from "@/utils/cn";
+import { Trash } from "lucide-react";
+import { useEffect } from "react";
+import { z } from "zod";
 
 import { Button } from "@/components/atoms/Button";
 import { Image } from "@/components/atoms/Image";
@@ -16,15 +20,22 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/atoms/Dialog";
-
-import { cn } from "@/utils/cn";
-import { Trash } from "lucide-react";
-import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+  useForm,
+} from "@/components/atoms/Form";
 
 const ACCEPTED_FILE_TYPES: Accept = {
   "image/jpeg": [".jpg", ".jpeg"],
   "image/png": [".png"],
 };
+
+const DESCRIPTION_ERROR_MESSAGE = "Description is required";
+const IMAGES_ERROR_MESSAGE = "At least one image is required";
 
 export type NewPostDialogProps = React.ComponentProps<typeof DialogContent> & {
   isOpen: boolean;
@@ -36,11 +47,37 @@ const NewPostDialog = ({
   className,
   ...props
 }: NewPostDialogProps) => {
-  const [files, setFiles] = useState<File[]>([]);
+  const [form, onFormSubmit] = useForm({
+    schema: z.object({
+      description: z
+        .string({
+          required_error: DESCRIPTION_ERROR_MESSAGE,
+        })
+        .min(1, DESCRIPTION_ERROR_MESSAGE),
+      images: z.array(z.instanceof(File)).min(1, IMAGES_ERROR_MESSAGE),
+    }),
+    submitHandler: async (data) => {
+      console.log(data);
+    },
+    options: {
+      defaultValues: {
+        images: [],
+      },
+    },
+  });
 
-  const onImageDeleteClick = (index: number) => {
-    setFiles((files) => files.filter((_, fileIndex) => fileIndex !== index));
+  const onImageDeleteClick = (images: File[], index: number) => {
+    form.setValue(
+      "images",
+      images.filter((_, fileIndex) => fileIndex !== index),
+    );
   };
+
+  useEffect(() => {
+    if (isOpen) {
+      form.reset();
+    }
+  }, [isOpen]);
 
   return (
     <Dialog
@@ -56,70 +93,105 @@ const NewPostDialog = ({
             Share with the world your last adventure!
           </DialogDescription>
         </DialogHeader>
-        <div className="flex flex-col gap-3">
-          <AspectRatio
-            ratio={1}
-            className="w-full overflow-hidden border border-dashed bg-background shadow-lg sm:rounded-md"
-          >
-            <Dropzone
-              accept={ACCEPTED_FILE_TYPES}
-              onDrop={(acceptedFiles) => setFiles(acceptedFiles)}
-              noClick={files?.length > 0}
-              multiple
-              className={cn(
-                "flex h-full w-full items-center justify-center",
-                files.length > 0 ? "cursor-default" : "cursor-pointer",
+        <Form {...form}>
+          <form className="flex flex-col gap-3">
+            <FormField
+              control={form.control}
+              name="images"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <AspectRatio
+                      ratio={1}
+                      className="w-full overflow-hidden border border-dashed bg-background shadow-lg sm:rounded-md"
+                    >
+                      <Dropzone
+                        accept={ACCEPTED_FILE_TYPES}
+                        onDrop={(acceptedFiles) =>
+                          field.onChange(acceptedFiles)
+                        }
+                        noClick={field.value.length > 0}
+                        multiple
+                        className={cn(
+                          "flex h-full w-full items-center justify-center",
+                          field.value.length > 0
+                            ? "cursor-default"
+                            : "cursor-pointer",
+                        )}
+                      >
+                        {({ isDragActive, acceptedTypes }) =>
+                          field.value.length > 0 ? (
+                            <Swiper
+                              className="h-full w-full"
+                              pagination
+                              onClick={(_, e) => {
+                                // e.preventDefault();
+                                e.stopPropagation();
+                              }}
+                            >
+                              {field.value.map((file, index) => (
+                                <SwiperSlide key={file.name}>
+                                  <Image
+                                    src={URL.createObjectURL(file)}
+                                    alt={""}
+                                    cover
+                                  />
+                                  <Button
+                                    onClick={(e) => {
+                                      e.preventDefault();
+                                      onImageDeleteClick(field.value, index);
+                                    }}
+                                    className="absolute bottom-2 right-2 z-20"
+                                    size="icon"
+                                    variant="outline"
+                                  >
+                                    <Trash className="h-5 w-5" />
+                                  </Button>
+                                </SwiperSlide>
+                              ))}
+                            </Swiper>
+                          ) : (
+                            <div className="flex flex-col gap-2 p-6 ">
+                              <Text className="text-center">
+                                {isDragActive
+                                  ? "Drop your images here"
+                                  : "Add your images"}
+                              </Text>
+                              <Small className="text-center">
+                                Accepting: {acceptedTypes?.join(" ")}
+                              </Small>
+                            </div>
+                          )
+                        }
+                      </Dropzone>
+                    </AspectRatio>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-            >
-              {({ isDragActive, acceptedTypes }) =>
-                files?.length > 0 ? (
-                  <Swiper
-                    className="h-full w-full"
-                    pagination
-                    onClick={(_, e) => {
-                      // e.preventDefault();
-                      e.stopPropagation();
-                    }}
-                  >
-                    {files.map((file, index) => (
-                      <SwiperSlide key={file.name}>
-                        <Image src={URL.createObjectURL(file)} alt={""} cover />
-                        <Button
-                          onClick={(e) => {
-                            e.preventDefault();
-                            onImageDeleteClick(index);
-                          }}
-                          className="absolute bottom-2 right-2 z-20"
-                          size="icon"
-                          variant="outline"
-                        >
-                          <Trash className="h-5 w-5" />
-                        </Button>
-                      </SwiperSlide>
-                    ))}
-                  </Swiper>
-                ) : (
-                  <div className="flex flex-col gap-2 p-6 ">
-                    <Text className="text-center">
-                      {isDragActive
-                        ? "Drop your images here"
-                        : "Add your images"}
-                    </Text>
-                    <Small className="text-center">
-                      Accepting: {acceptedTypes?.join(" ")}
-                    </Small>
-                  </div>
-                )
-              }
-            </Dropzone>
-          </AspectRatio>
-          <Textarea
-            placeholder="Write about your adventure..."
-            className="h-20 resize-none"
-          />
-        </div>
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      className="h-20 resize-none"
+                      placeholder="Write about your adventure..."
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
         <DialogFooter className="sm:justify-start">
-          <Button type="button">Add post</Button>
+          <Button type="submit" onClick={onFormSubmit}>
+            Add post
+          </Button>
           <DialogClose asChild>
             <Button type="button" variant="secondary">
               Close
