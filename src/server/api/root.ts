b3dedfae6a8a1fc49c24e88821fb4a75c1e5440e
Karol-Waliszewski <karolwaliszewski@gmail.com>
2023-12-22 19:51:04 +0100

    Added s3 integration with trpc

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 import { userRouter } from "@/server/api/routers/user";
 import { postRouter } from "@/server/api/routers/post";
+import { storageRouter } from "@/server/api/routers/storage";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { postRouter } from "@/server/api/routers/post";
 export const appRouter = createTRPCRouter({
   post: postRouter,
   user: userRouter,
+  storage: storageRouter,
 });
 
 // export type definition of API
